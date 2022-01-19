e652923585a55ca2903ece03b1300d1451fe0626
Max Franz <maex.payn@googlemail.com>
2022-01-19 20:56:10 +0100

    update template names

--- test/src/greeter.cpp
+++ test/src/greeter.cpp
@@ -1,6 +1,6 @@
 #include <doctest/doctest.h>
-#include <greeter/greeter.h>
-#include <greeter/version.h>
+#include <ronin/greeter.h>
+#include <ronin/version.h>
 
 #include <string>
 
@@ -15,7 +15,7 @@ TEST_CASE("Greeter") {
   CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
 }
 
-TEST_CASE("Greeter version") {
-  static_assert(std::string_view(GREETER_VERSION) == std::string_view("1.0"));
-  CHECK(std::string(GREETER_VERSION) == std::string("1.0"));
+TEST_CASE("Ronin version") {
+  static_assert(std::string_view(RONIN_VERSION) == std::string_view("1.0"));
+  CHECK(std::string(RONIN_VERSION) == std::string("1.0"));
 }
